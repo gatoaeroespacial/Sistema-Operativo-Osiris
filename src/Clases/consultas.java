222169c30249bdf16b925886c98aa712ff5c5e1f
gatoaeroespacial <juanmauricio6789@gmail.com>
2024-06-13 23:32:06 -0500

    hola2

--- src/Clases/consultas.java
+++ src/Clases/consultas.java
@@ -1,4 +1,4 @@
-    package Clases;
+package Clases;
 
 import Formularios.Escritorio;
 import java.sql.Connection;
@@ -11,67 +11,70 @@ import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 public class consultas {
-    public void guardarUsuario(String usuario, String password, JFrame frame){
+
+    public void guardarUsuario(String usuario, String password, JFrame frame) {
         ConexionDB db = new ConexionDB();
-        String sql = "insert into usuarios(nombre, clave) values ('" + usuario +"', '" + password +"');";
+        String sql = "insert into usuarios(nombre, clave) values ('" + usuario + "', '" + password + "');";
         Statement st;
         Connection conexion = db.conectar();
-        if (usuario.length()<5){
-          JOptionPane.showMessageDialog(null, "Usuario no Guardado, el usuario ingresado debe tener como minimo 5 caracteres");
-          return;
-        }else if(password.length()<4){
-              JOptionPane.showMessageDialog(null, "Usuario no Guardado, la contraseña ingresada debe tener como minimo 5 caracteres");
-          return;
+        if (usuario.length() < 5) {
+            JOptionPane.showMessageDialog(null, "Usuario no Guardado, el usuario ingresado debe tener como minimo 5 caracteres");
+            return;
+        } else if (password.length() < 4) {
+            JOptionPane.showMessageDialog(null, "Usuario no Guardado, la contraseña ingresada debe tener como minimo 5 caracteres");
+            return;
         }
-        try
-        {
+        try {
             st = conexion.createStatement();
             int rs = st.executeUpdate(sql);
             JOptionPane.showMessageDialog(null, "Guardado correctamente");
-             // Esto supone que estás llamando a guardarUsuario() desde un objeto dentro del formulario
-        frame.dispose();
-        }catch(SQLException e)
-        {
+            // Esto supone que estás llamando a guardarUsuario() desde un objeto dentro del formulario
+            frame.dispose();
+        } catch (SQLException e) {
             System.out.println(e);
         }
     }
-    
-    public void consultarUsuario(String user, String pass, JFrame frame)
-    {
+
+    public void consultarUsuario(String user, String pass, JFrame frame) {
         // TODO add your handling code here:
         ConexionDB db = new ConexionDB();
         // Se inicializa a null
         String usuarioCorrecto = null;
         String passCorrecto = null;
-    try {
+        try {
 
-        Connection cn = db.conectar();
-        PreparedStatement pst = cn.prepareStatement("SELECT nombre, clave FROM usuarios where nombre ='"+ user +"' and clave = '"+pass+"'");
-        ResultSet rs = pst.executeQuery();
-        System.out.println(rs);
-      
+            Connection cn = db.conectar();
+            PreparedStatement pst = cn.prepareStatement("SELECT nombre, clave, init, idUsuario FROM usuarios where nombre ='" + user + "' and clave = '" + pass + "'");
+            ResultSet rs = pst.executeQuery();
+            System.out.println(rs);
 
-        if (rs.next()) {
-            usuarioCorrecto = rs.getString(1);
-            passCorrecto = rs.getString(2);
-            
-        } 
-         
-        if (user.equals(usuarioCorrecto) && pass.equals(passCorrecto)) {
-            Escritorio esc=new Escritorio();
-            esc.setVisible(true);
-          // JOptionPane.showMessageDialog(null, "Login correcto Bienvenido " + user);
-            frame.dispose();
-            
-            
-        } else if (!user.equals(usuarioCorrecto) || pass.equals(passCorrecto)) {
-         
-            JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos");
+            if (rs.next()) {
+                usuarioCorrecto = rs.getString(1);
+                passCorrecto = rs.getString(2);
+
+            }
+
+            if (user.equals(usuarioCorrecto) && pass.equals(passCorrecto)) {
+                System.out.println(rs.getString(3));
+                if (rs.getString(3).equals("NO")) {
+                    pst = cn.prepareStatement("UPDATE usuarios SET init = 'SI' WHERE idUsuario= '" + rs.getString(4) + "';");
+                    rs = pst.executeQuery();
+                    System.out.println(rs);
+                }
+
+                Escritorio esc = new Escritorio();
+                esc.setVisible(true);
+                // JOptionPane.showMessageDialog(null, "Login correcto Bienvenido " + user);
+                frame.dispose();
+
+            } else if (!user.equals(usuarioCorrecto) || pass.equals(passCorrecto)) {
+
+                JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos");
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error " + e);
         }
+    }
 
-    } catch (Exception e) {
-        JOptionPane.showMessageDialog(null, "Error " + e);
-    }
-    }
-    
 }
